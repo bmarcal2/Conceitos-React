2b085174bd63284e5ab748c73652b866c5cc2458
BERNARDO MARÇAL COSTA <bernardo_costa2@estudante.sesisenai.org.br>
2026-05-14 12:30:53 -0300

    front

--- src/paginas/Usuário.jsx
+++ src/paginas/Usuário.jsx
@@ -2,9 +2,19 @@ import { useEffect, useState } from "react"
 
 export default function Usuário () {
     const [contador, setContador] = useState(0)
+    const [usuarios, setUsuarios] = useState([])
 
     useEffect(() => {
         document.title = 'contagem ' + contador
+        const buscarUuarios = async () => {
+            const resposta = await fetch ('http://localhost:3000/usuarios')
+            const data = await resposta.json()
+            setUsuarios(data)
+
+            console.log(data);
+
+        }
+
     }, [contador])
 
     return (
@@ -13,6 +23,15 @@ export default function Usuário () {
         <h1>{contador}</h1>
 
         <button onClick={() => {setContador(contador + 1)}}>somar</button>
+
+        <ul>
+        {usuarios.map( (usuario) => {
+            <li key ={usuario.id}>
+                <b>{usuario.nome}</b>
+                STATUS: { usuario.ativo ? 'Ativo' : 'Desativo'}
+            </li>
+        })}
+        </ul>
     </div>
       
     )
